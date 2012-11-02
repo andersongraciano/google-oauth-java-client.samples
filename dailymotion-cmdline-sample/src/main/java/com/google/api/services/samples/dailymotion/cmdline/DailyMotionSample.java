289b6a1112c05868649589d10a0c3d4a2f2a38ef
Ravi Mistry <rmistry@google.com>
2012-11-02 15:06:40 -0400

    Update google-oauth-java-client sample to 1.12.0-beta https://codereview.appspot.com/6814059/

--- dailymotion-cmdline-sample/src/main/java/com/google/api/services/samples/dailymotion/cmdline/DailyMotionSample.java
+++ dailymotion-cmdline-sample/src/main/java/com/google/api/services/samples/dailymotion/cmdline/DailyMotionSample.java
@@ -28,7 +28,7 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
-import com.google.api.client.json.jackson.JacksonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 
 import java.awt.Desktop;
 import java.awt.Desktop.Action;
