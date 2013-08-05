40d024a3a787c00c6fe9d1b9124c5d209041cef3
Nick Miceli <ngmiceli@google.com>
2013-08-05 12:09:21 -0400

    oauth samples: Update sample to 1.16.0-rc https://codereview.appspot.com/11528043/

--- dailymotion-cmdline-sample/src/main/java/com/google/api/services/samples/dailymotion/cmdline/DailyMotionSample.java
+++ dailymotion-cmdline-sample/src/main/java/com/google/api/services/samples/dailymotion/cmdline/DailyMotionSample.java
@@ -19,7 +19,6 @@ import com.google.api.client.auth.oauth2.BearerToken;
 import com.google.api.client.auth.oauth2.ClientParametersAuthentication;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.java6.auth.oauth2.FileCredentialStore;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
@@ -30,8 +29,9 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -43,6 +43,16 @@ import java.util.Arrays;
  * @author Ravi Mistry
  */
 public class DailyMotionSample {
+
+  /** Directory to store user credentials. */
+  private static final java.io.File DATA_STORE_DIR =
+      new java.io.File(System.getProperty("user.home"), ".store/dailymotion_sample");
+
+  /**
+   * Global instance of the {@link DataStoreFactory}. The best practice is to make it a single
+   * globally shared instance across your application.
+   */
+  private static FileDataStoreFactory DATA_STORE_FACTORY;
 
   /** OAuth 2 scope. */
   private static final String SCOPE = "read";
@@ -60,9 +70,6 @@ public class DailyMotionSample {
   /** Authorizes the installed application to access user's protected data. */
   private static Credential authorize() throws Exception {
     OAuth2ClientCredentials.errorIfNotSpecified();
-    // set up file credential store
-    FileCredentialStore credentialStore = new FileCredentialStore(
-        new File(System.getProperty("user.home"), ".credentials/dailymotion.json"), JSON_FACTORY);
     // set up authorization code flow
     AuthorizationCodeFlow flow = new AuthorizationCodeFlow.Builder(BearerToken
         .authorizationHeaderAccessMethod(),
@@ -73,7 +80,7 @@ public class DailyMotionSample {
             OAuth2ClientCredentials.API_KEY, OAuth2ClientCredentials.API_SECRET),
         OAuth2ClientCredentials.API_KEY,
         AUTHORIZATION_SERVER_URL).setScopes(Arrays.asList(SCOPE))
-        .setCredentialStore(credentialStore).build();
+        .setDataStoreFactory(DATA_STORE_FACTORY).build();
     // authorize
     LocalServerReceiver receiver = new LocalServerReceiver.Builder().setHost(
         OAuth2ClientCredentials.DOMAIN).setPort(OAuth2ClientCredentials.PORT).build();
@@ -106,6 +113,7 @@ public class DailyMotionSample {
 
   public static void main(String[] args) {
     try {
+      DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
       final Credential credential = authorize();
       HttpRequestFactory requestFactory =
           HTTP_TRANSPORT.createRequestFactory(new HttpRequestInitializer() {
